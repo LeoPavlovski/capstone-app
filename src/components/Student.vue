df24a2f2660717ad46ab396285b422df4c1a2682
LeoPavlovski <lp29379@seeu.edu.mk>
2024-10-23 09:55:24 +0200

    fix : 1. Added the commit for the login  ( previous work )       2. Fixed the course date ( now each time the courses will be displayed based on the day that they were created).       3. Data table fixes ( with the date , etc )       4. Functionality when a student joins a course the max_students goes 1 lower.       5. fixed the start_date with the current date in the function. Retrieves the date created.       6. Duration must be a time value ( number )       7. Deadline needs to be handled for the internship. Make sure it is in the time period.

--- src/components/Student.vue
+++ src/components/Student.vue
@@ -12,36 +12,40 @@
                     <tr>
                       <td>{{item.name}}</td>
                       <td>{{item.course_code}}</td>
-                      <td>
-                        <div class="d-flex" style="width:100px;">
-                          <v-icon color="primary">mdi-calendar-today</v-icon>
-                          {{item.start_date}}
-                        </div>
-                      </td>
-                      <td>
-                        <div class="d-flex" style="width:100px;">
-                          <v-icon color="red">mdi-calendar-clock</v-icon>
-                          {{item.end_date}}
-                        </div>
-                      </td>
+<!--                      <td>-->
+<!--                        <div class="d-flex" style="width:100px;">-->
+<!--                          <v-icon color="primary">mdi-calendar-today</v-icon>-->
+<!--                          {{item.start_date}}-->
+<!--                        </div>-->
+<!--                      </td>-->
+<!--                      <td>-->
+<!--                        <div class="d-flex" style="width:100px;">-->
+<!--                          <v-icon color="red">mdi-calendar-clock</v-icon>-->
+<!--                          {{item.end_date}}-->
+<!--                        </div>-->
+<!--                      </td>-->
+                      <td class="text-left">{{ getDayOfWeek(item.start_date) }}</td>
                       <td>
                         <div class="d-flex">
                           <v-icon color="orange">mdi-clock</v-icon>
                           {{item.time}}
                         </div>
                       </td>
-                      <td class="d-flex align-center">
-                        <div class="d-flex" style="width:100px;">
+                      <td>
+                        <div class="d-flex" style="width:130px;">
                           <v-icon color="purple">mdi-google-maps</v-icon>
                           {{item.location}}
                         </div>
 
                       </td>
                       <td>
-                        <v-icon color="green">mdi-account-multiple</v-icon>
-                        {{item.max_students}}</td>
+                        <div>
+                          <v-icon color="green">mdi-account-multiple</v-icon>
+                          {{item.max_students}}
+                        </div>
+                      </td>
                       <td>
-                        <v-btn class="green mr-2 white--text" @click="applyCourse(item)" small>Apply</v-btn>
+                        <v-btn :disabled="item.max_students === 0" class="green mr-2 white--text" @click="applyCourse(item)" small>Apply</v-btn>
                       </td>
                     </tr>
                   </template>
@@ -50,8 +54,16 @@
             </v-col>
           <v-card  color="primary" class="mt-5">
             <v-card-title class="white--text">My Courses</v-card-title>
-            <v-data-table height="420" :items="myCourses" :headers="courseHeaders">
-
+            <v-data-table height="420" :items="myCourses" :headers="myCoursesHeaders">
+              <template v-slot:item="{item}">
+                <tr>
+                  <td class="text-left">{{item.name}}</td>
+                  <td class="text-left">{{item.course_code}}</td>
+                  <td class="text-left">{{ getDayOfWeek(item.start_date) }}</td>
+                  <td class="text-left">{{ item.time.slice(0, 5) }}</td>
+                  <td class="text-left">{{item.location}}</td>
+                </tr>
+              </template>
             </v-data-table>
           </v-card>
         </v-col>
@@ -74,19 +86,19 @@
                 </div>
               </template>
               <template v-slot:item.start_date="{item}">
-                <div class="d-flex" style="width:100px;">
-                  <v-icon color="primary">mdi-calendar-today</v-icon>
+                <div class="d-flex" style="width:120px;">
+                  <v-icon color="primary" class="mr-2">mdi-calendar-today</v-icon>
                   {{item.start_date}}
                 </div>
               </template>
               <template v-slot:item.description="{item}">
-                <div class="d-flex" style="width:200px;">
+                <div class="d-flex" style="width:100px;">
                   {{item.description}}
                 </div>
               </template>
               <template v-slot:item.end_date="{item}">
-                <div class="d-flex" style="width:100px;">
-                  <v-icon color="red">mdi-calendar-clock</v-icon>
+                <div class="d-flex" style="width:130px;">
+                  <v-icon color="red" class="mr-2">mdi-calendar-clock</v-icon>
                   {{item.end_date}}
                 </div>
               </template>
@@ -102,9 +114,21 @@
                 </div>
               </template>
               <template v-slot:item.deadline="{item}">
-                <div class="d-flex" style="width:120px;">
-                  <span>{{item.deadline}}</span>
-                </div>
+                  <div class="d-flex" style="width:120px;">
+                    <v-tooltip bottom>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-chip
+                          class="white--text"
+                          v-bind="attrs"
+                          v-on="on"
+                          :color="isDeadlineViable(item.deadline) ? 'green' : 'red'"
+                      >
+                        {{ item.deadline }}
+                      </v-chip>
+                    </template>
+                    <span>{{ isDeadlineViable(item.deadline) ? 'Available' : 'Deadline passed' }}</span>
+                  </v-tooltip>
+                  </div>
               </template>
               <template v-slot:item.courses="{ item }">
                 <ul>
@@ -115,8 +139,16 @@
           </v-card>
           <v-card  color="primary" class="mt-8">
             <v-card-title class="white--text">Today's Lectures</v-card-title>
-            <v-data-table height="420" :items="filteredCoursesToday" :headers="courseHeaders">
-
+            <v-data-table height="420" :items="filteredCoursesByWeekday" :headers="myCoursesHeaders">
+              <template v-slot:item="{item}">
+                <tr>
+                  <td class="text-left">{{item.name}}</td>
+                  <td class="text-left">{{item.course_code}}</td>
+                  <td class="text-left">{{ getDayOfWeek(item.start_date) }}</td>
+                  <td class="text-left">{{ item.time.slice(0, 5) }}</td>
+                  <td class="text-left">{{item.location}}</td>
+                </tr>
+              </template>
             </v-data-table>
           </v-card>
         </v-col>
@@ -137,6 +169,7 @@ export default {
     await this.getInternships();
     await this.getStudentCourses();
     await this.getStudentInvitations();
+    console.log('myCourses : ' , this.myCourses);
   },
   components: {
     Navigation,
@@ -171,10 +204,14 @@ export default {
     joinedCourses() {
       return this.myCourses;
     },
-    filteredCoursesToday() {
-      const today = new Date().toISOString().split('T')[0]; // Get today's date in YYYY-MM-DD format
+    filteredCoursesByWeekday() {
+      const today = new Date().getDay(); // Get today's day of the week (0 = Sunday, 1 = Monday, etc.)
+
       return this.myCourses.filter(course => {
-        return course.start_date === today || course.end_date === today;
+        const courseStartDate = new Date(course.start_date); // Parse the course's start date
+        const courseDay = courseStartDate.getDay(); // Get the weekday for the course's start date
+
+        return courseDay === today; // Return only courses where the start date matches today's day of the week
       });
     },
     isFormValid() {
@@ -220,30 +257,48 @@ export default {
         { id: 0, name: "No" }
       ],
       internshipHeaders: [
-        { text: "Created", value: "created" },
-        { text: "Internship Name", value: "name" },
-        { text: "Company", value: "company" },
-        { text: "Description", value: "description" },
-        { text: "Start Date", value: "start_date" },
-        { text: "End Date", value: "end_date" },
+        { text: "Created", value: "created", sortable:false, },
+        { text: "Internship", value: "name",sortable:false, },
+        { text: "Company", value: "company", sortable:false,  },
+        { text: "Description", value: "description", sortable:false,  },
+        { text: "Start", value: "start_date" , sortable:false,  },
+        { text: "End", value: "end_date" , sortable:false,  },
+        { text: "Location", value: "location" , sortable:false,  },
+        { text: "Duration", value: "duration" , sortable:false,  },
+        { text: "Scholarship", value: "stipend" , sortable:false,  },
+        { text: "Deadline", value: "deadline", sortable:false,  },
+      ],
+      myCoursesHeaders: [
+        { text: "Course Name", value: "name" },
+        { text: "Course Code", value: "course_code" },
+        { text: "Day", value: "day" },
+        { text: "Time", value: "time" },
         { text: "Location", value: "location" },
-        { text: "Duration", value: "duration" },
-        { text: "Scholarship", value: "stipend" },
-        { text: "Deadline", value: "deadline" },
       ],
       courseHeaders: [
         { text: "Course Name", value: "name" },
         { text: "Course Code", value: "course_code" },
-        { text: "Start Date", value: "start_date" },
-        { text: "End Date", value: "end_date" },
+        { text: "Day", value: "day" },
         { text: "Time", value: "time" },
         { text: "Location", value: "location" },
-        { text: "Max Students", value: "max_students" },
+        { text: "Available", value: "max_students", sortable:false, width:100 },
+        { text: "Apply", value: "Apply", sortable:false, width:100 },
       ],
       courseItem: null,
     };
   },
   methods: {
+    isDeadlineViable(deadline) {
+      const today = new Date(); // Get today's date
+      const deadlineDate = new Date(deadline); // Convert the deadline string to a Date object
+
+      return today <= deadlineDate; // Return true if the deadline hasn't passed, false otherwise
+    },
+    getDayOfWeek(dateString) {
+      const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      const date = new Date(dateString);
+      return daysOfWeek[date.getDay()];
+    },
     getNews(){
       this.$store.dispatch('getNews');
     },
