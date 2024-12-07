2fb7a4cfcfe6e67f99cfc10238fede331bb8687f
LeoPavlovski <lp29379@seeu.edu.mk>
2024-12-07 15:05:55 +0100

    fix : Validation on login screen.

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,138 +4,160 @@
       <v-col cols="12" sm="12">
         <h3 class="text-center">Welcome to Internship Finder 📝</h3>
         <v-card class="elevation-6 mt-3" height="500">
+          <v-snackbar v-model="snackbar" :color="snackbarColor" timeout="3000">
+            {{ snackbarMessage }}
+          </v-snackbar>
           <v-window v-model="step" >
             <v-window-item :value="1">
               <v-row>
-                <v-col cols="12" md="12">
-                  <v-card-text>
-                    <h4 class="text-center" style="font-size:15px;">Login to Your Account</h4>
-                    <h6 class="text-center  mt-2" style="font-size:15px;">Log in to your account so you can continue building <br>and editing your onboarding flows</h6>
-                    <v-row align="center" justify="center">
-                      <v-col cols="12" sm="12">
-                        <div class="px-3 mt-10">
-                          <v-text-field
-                              label="Email"
-                              v-model="email"
-                              dense
-                              prepend-inner-icon="mdi-email mdi-18px mt-1 red--text"
-                              color="blue"
-                              autocomplete="false"
-                              class="mt-5"
-
-                          />
-                          <v-text-field
-                              label="Password"
-                              dense
-                              v-model="password"
-                              prepend-inner-icon="mdi-key mdi-18px mt-1 red--text"
-                              color="blue"
-                              autocomplete="false"
-                              type="password"
-                          />
-
-                        </div>
-                        <div class="mx-2 mt-2">
-                          <v-btn  @click="performLogin" color="blue" dark rounded block>Log in</v-btn>
-                        </div>
-
-                      </v-col>
-                    </v-row>
-                  </v-card-text>
-                      <v-card-text class="text-center mt-7">
-                        <h3 class="" style="font-size:14px;">Don't Have an Account Yet?</h3>
-                        <h6 class=" mt-1" style="font-size:14px;">Let's get you all set up so you can<br> start creating your your first onboarding experience</h6>
-                          <v-btn width="30%" small color="red white--text" class="mt-7" dark rounded outlined  @click="step++">SIGN UP</v-btn>
-                      </v-card-text>
-
-                </v-col>
-              </v-row>
-            </v-window-item>
-            <v-window-item :value="2">
-              <v-row dense>
-                <v-col cols="12" md="12">
-                  <div>
+                <v-col cols="12" md="12" lg="12" class="mx-auto">
+                  <v-card class="pa-6" >
                     <v-card-text>
-                      <h3 class="text-center mb-2 " style="font-size:16px;">Already Signed up?</h3>
-                      <h6 class="text-center" style="font-size:16px;">Log in to your account so you can continue <br> building  and editing your onboarding flows</h6>
+                      <!-- Title -->
+                      <h4 class="text-center mb-4" style="font-size: 24px; font-weight: 600; color: #2c3e50;">Login to Your Account</h4>
+                      <h6 class="text-center mb-6" style="font-size: 16px; color: #7f8c8d;">Log in to your account to continue building <br> and editing your onboarding flows.</h6>
+
+                      <!-- Form -->
+                      <v-form @submit.prevent="performLogin">
+                        <v-row class="mb-4">
+                          <v-col cols="12">
+                            <v-text-field
+                                label="Email"
+                                v-model="email"
+                                dense
+                                prepend-inner-icon="mdi-email mdi-18px"
+                                color="blue"
+                                autocomplete="off"
+                                class="mt-3"
+                                outlined
+                                required
+                            />
+                          </v-col>
+                        </v-row>
+
+                        <v-row class="mb-2 mt-n10">
+                          <v-col cols="12">
+                            <v-text-field
+                                label="Password"
+                                v-model="password"
+                                dense
+                                prepend-inner-icon="mdi-key mdi-18px"
+                                color="blue"
+                                autocomplete="off"
+                                type="password"
+                                outlined
+                                required
+                            />
+                          </v-col>
+                        </v-row>
+
+                        <!-- Login Button -->
+                        <v-btn @click="performLogin" color="blue" dark rounded block large>
+                          Log in
+                        </v-btn>
+                      </v-form>
+
+                      <!-- Signup Prompt -->
+                      <v-row class="mt-5 text-center">
+                        <v-col>
+                          <h6 style="font-size: 14px;">Don't have an account?</h6>
+                          <h5  style="font-size: 14px;">Please reach out to an admin for assistance.</h5>
+<!--                          <v-btn @click="step++" color="blue" text>-->
+<!--                            Sign Up-->
+<!--                          </v-btn>-->
+                        </v-col>
+                      </v-row>
                     </v-card-text>
-                    <div class="text-center px-6">
-                      <v-btn color="red white--text"  dark rounded outlined  block  @click="step--">Log in</v-btn>
-                    </div>
-                  </div>
-
-                  <v-card-text>
-                    <h4 class="text-center" style="font-size:16px;">Sign Up for an Account</h4>
-                    <h6 class="text-center mt-3 mb-3 grey--text " style="font-size:16px;">Let's get you all set up so you can start creating your <br>first onboarding experience</h6>
-                    <v-row align="center" justify="center">
-                      <v-col cols="6" style="width:20px;">
-                        <v-text-field
-                            label="Name"
-                            outlined
-                            dense
-                            color="blue"
-                            autocomplete="false"
-                            v-model="firstName"
-                        />
-                        <v-text-field
-                            label="Surname"
-                            outlined
-                            dense
-                            color="blue"
-                            autocomplete="false"
-                            v-model="lastName"
-                        />
-                      </v-col>
-                      <v-col cols="6" style="width:20px;">
-                        <v-text-field
-                            label="Email"
-                            outlined
-                            dense
-                            color="blue"
-                            autocomplete="false"
-                            v-model="email"
-                        />
-                        <v-text-field
-                            label="Password"
-                            outlined
-                            dense
-                            color="blue"
-                            autocomplete="false"
-                            type="password"
-                            v-model="password"
-                        />
-                      </v-col>
-                      <v-select
-                          v-model="staff"
-                          :items="users"
-                          item-text="role"
-                          item-value="id"
-                          dense
-                          outlined
-                          class="px-3 mt-n2"
-                          label="Select Role"
-                      >
-                      </v-select>
-                      <v-select
-                          v-model="department"
-                          :items="departments"
-                          item-text="departmentName"
-                          item-value="id"
-                          dense
-                          outlined
-                          class="px-3 mt-n2"
-                          label="Select Department"
-                      >
-                      </v-select>
-                    </v-row>
-                  </v-card-text>
-                  <div class="d-flex align-center justify-center ">
-                    <v-btn class="mb-5"  small @click="performRegister" color="blue" dark rounded>Sign up</v-btn>
-                  </div>
+                  </v-card>
                 </v-col>
-
               </v-row>
+
             </v-window-item>
+<!--            <v-window-item :value="2">-->
+<!--              <v-row dense>-->
+<!--                <v-col cols="12" md="12">-->
+<!--                  <div>-->
+<!--                    <v-card-text>-->
+<!--                      <h3 class="text-center mb-2 " style="font-size:16px;">Already Signed up?</h3>-->
+<!--                      <h6 class="text-center" style="font-size:16px;">Log in to your account so you can continue <br> building  and editing your onboarding flows</h6>-->
+<!--                    </v-card-text>-->
+<!--                    <div class="text-center px-6">-->
+<!--                      <v-btn color="red white&#45;&#45;text"  dark rounded outlined  block  @click="step&#45;&#45;">Log in</v-btn>-->
+<!--                    </div>-->
+<!--                  </div>-->
+
+<!--                  <v-card-text>-->
+<!--                    <h4 class="text-center" style="font-size:16px;">Sign Up for an Account</h4>-->
+<!--                    <h6 class="text-center mt-3 mb-3 grey&#45;&#45;text " style="font-size:16px;">Let's get you all set up so you can start creating your <br>first onboarding experience</h6>-->
+<!--                    <v-row align="center" justify="center">-->
+<!--                      <v-col cols="6" style="width:20px;">-->
+<!--                        <v-text-field-->
+<!--                            label="Name"-->
+<!--                            outlined-->
+<!--                            dense-->
+<!--                            color="blue"-->
+<!--                            autocomplete="false"-->
+<!--                            v-model="firstName"-->
+<!--                        />-->
+<!--                        <v-text-field-->
+<!--                            label="Surname"-->
+<!--                            outlined-->
+<!--                            dense-->
+<!--                            color="blue"-->
+<!--                            autocomplete="false"-->
+<!--                            v-model="lastName"-->
+<!--                        />-->
+<!--                      </v-col>-->
+<!--                      <v-col cols="6" style="width:20px;">-->
+<!--                        <v-text-field-->
+<!--                            label="Email"-->
+<!--                            outlined-->
+<!--                            dense-->
+<!--                            color="blue"-->
+<!--                            autocomplete="false"-->
+<!--                            v-model="email"-->
+<!--                        />-->
+<!--                        <v-text-field-->
+<!--                            label="Password"-->
+<!--                            outlined-->
+<!--                            dense-->
+<!--                            color="blue"-->
+<!--                            autocomplete="false"-->
+<!--                            type="password"-->
+<!--                            v-model="password"-->
+<!--                        />-->
+<!--                      </v-col>-->
+<!--                      <v-select-->
+<!--                          v-model="staff"-->
+<!--                          :items="users"-->
+<!--                          item-text="role"-->
+<!--                          item-value="id"-->
+<!--                          dense-->
+<!--                          outlined-->
+<!--                          class="px-3 mt-n2"-->
+<!--                          label="Select Role"-->
+<!--                      >-->
+<!--                      </v-select>-->
+<!--                      <v-select-->
+<!--                          v-model="department"-->
+<!--                          :items="departments"-->
+<!--                          item-text="departmentName"-->
+<!--                          item-value="id"-->
+<!--                          dense-->
+<!--                          outlined-->
+<!--                          class="px-3 mt-n2"-->
+<!--                          label="Select Department"-->
+<!--                      >-->
+<!--                      </v-select>-->
+<!--                    </v-row>-->
+<!--                  </v-card-text>-->
+<!--                  <div class="d-flex align-center justify-center ">-->
+<!--                    <v-btn class="mb-5"  small @click="performRegister" color="blue" dark rounded>Sign up</v-btn>-->
+<!--                  </div>-->
+<!--                </v-col>-->
+
+<!--              </v-row>-->
+<!--            </v-window-item>-->
           </v-window>
         </v-card>
       </v-col>
@@ -150,6 +172,12 @@ import {mapGetters, mapState} from "vuex";
 
 export default {
   data: () => ({
+    // this.snackbarMessage = error.response.data.message;
+    // this.snackbarColor = 'error';
+    // this.snackbar = true;
+    snackbarColor:'error',
+    snackbar:false,
+    snackbarMessage:'',
     departments:[{id:1,departmentName:'Computer Science'},{id:2,departmentName:'Business Economics'}],
     department:null,
     role:null,
@@ -188,34 +216,42 @@ export default {
           this.$router.push('/companies');
         }
         localStorage.setItem('department', this.user.department);
+        this.snackbarMessage = 'Success';
+        this.snackbarColor = 'success';
+        this.snackbar = true;
+      }).catch((error)=>{
+        this.snackbarMessage = error;
+        console.log('message' , this.snackbarMessage);
+        this.snackbarColor = 'error';
+        this.snackbar = true;
       })
       },
-    performRegister(){
-      if(this.staff === 1){
-        this.role =  'Student'
-      }else if(this.staff ===2){
-        this.role =  'Professor'
-      }else if(this.staff ===3){
-        this.role =  'Admin'
-      }
-      //const intStaff = parseInt(this.staff);
-      const department = parseInt(this.department);
-      const body ={
-        email:this.email,
-        password:this.password,
-        roleId : parseInt(this.staff),
-        roleName : this.role,
-        name:this.firstName,
-        surname:this.lastName,
-        department:parseInt(this.department),
-        departmentName:department === 1? this.departments[0].departmentName : this.departments[1].departmentName
-      }
-
-      this.$store.dispatch('register',body).then(res=>{
-        this.step=0;
-      })
-
-    }
+    // performRegister(){
+    //   if(this.staff === 1){
+    //     this.role =  'Student'
+    //   }else if(this.staff ===2){
+    //     this.role =  'Professor'
+    //   }else if(this.staff ===3){
+    //     this.role =  'Admin'
+    //   }
+    //   //const intStaff = parseInt(this.staff);
+    //   const department = parseInt(this.department);
+    //   const body ={
+    //     email:this.email,
+    //     password:this.password,
+    //     roleId : parseInt(this.staff),
+    //     roleName : this.role,
+    //     name:this.firstName,
+    //     surname:this.lastName,
+    //     department:parseInt(this.department),
+    //     departmentName:department === 1? this.departments[0].departmentName : this.departments[1].departmentName
+    //   }
+    //
+    //   this.$store.dispatch('register',body).then(res=>{
+    //     this.step=0;
+    //   })
+    //
+    // }
   },
   props: {
     source: String
